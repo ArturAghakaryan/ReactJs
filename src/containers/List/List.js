12e2ec868a7191255bfd016160b7f4794feec263
artur <artur@weem.tech>
2020-12-30 16:42:21 +0400

    homework 1

---
+++ src/containers/List/List.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./List.scss";
+
+import ListItem from "../../components/ListItem/ListItem";
+
+function List() {
+    
+    return (
+        <div className="container">
+            <div className="list--items">
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+            </div>
+        </div>
+    )
+    
+
+}
+
+
+export default List;
