fbd3f0b6756dea2ebd7783b9cab806ee25ea0e38
artur <artur@weem.tech>
2021-01-09 17:09:40 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.scss';
 
-import Authentication from "./containers/Authentication/Authentication";
 
 function App() {
   return (
     <div className="App">
-        <main className="content--main">
-          <Authentication />
-        </main>
+        
     </div>
   );
 }
