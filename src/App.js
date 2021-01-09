c11aad684f39c796b96092f47d55a69e56469bcd
artur <artur@weem.tech>
2021-01-09 20:29:07 +0400

    crate layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.scss';
 
+import Layout from "./components/Layout/Layout"
+import Header from "./containers/Header/Header"
+import Footer from "./containers/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
-        
+        <Header />
+        <Layout>
+          <div className="content-main">
+            <section className="welcome-content">
+              <div className="welcome-bg"></div>
+              <div className="welcome-info">
+                <div className="welcome-info-content">
+                  <h2>Welcome !</h2>
+                  <p>my first React project</p>
+                </div>
+              </div>
+            </section>
+          </div>
+        </Layout>
+        <Footer />
     </div>
   );
 }
